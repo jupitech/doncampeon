61e84643440ed2c0f2f6d2c6823fa74e93417d54
jupitech <jupitech.dev@gmail.com>
2016-03-20 14:59:28 -0600

    Actua

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,6 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
                     <div class="panel-body">
                         {!! Form::open(['route' => 'login', 'class' => 'form']) !!}
                             <div class="form-group">
